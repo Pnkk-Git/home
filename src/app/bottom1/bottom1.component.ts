7b6a3871d69db20aaa9e9d425b8dab0ddb84d8dc
Pnkk-Git <office@pnkk.in>
2024-06-17 00:13:33 +0530

    intial commit

---
+++ src/app/bottom1/bottom1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom1',
+  templateUrl: './bottom1.component.html',
+  styleUrls: ['./bottom1.component.css']
+})
+export class Bottom1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
