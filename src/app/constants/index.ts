cccf4f16623db611bff8c62db2bfc546b6b22ae0
Pnkk-Git <office@pnkk.in>
2024-07-15 17:45:22 +0530

    index file

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,64 +1,190 @@
 export const SERVICES = [
     {
         "id": 1,
-        "imageUrl": "bachelor-party.jpg",
-        "description": "A celebration for a soon-to-be groom organized and attended primarily by male friends, often on the night before or in the days leading up to the wedding, and ranging from a formal gathering to a night of drinking and entertainment at a strip joint.",
-        "title": "Bachelor party",
-        "category": "B"
+        "imageUrl": "anniversary0.jpg",
+        "description": "An anniversary is a celebration of a significant date that marks an event from the past, such as a wedding or the founding of a company. It is a time for reflection and celebration, often involving family and friends. Common anniversary celebrations include renewing vows, throwing a party, or taking a special trip. The event is usually marked by exchanging gifts, enjoying a special meal, and recalling cherished memories. Anniversaries can be personal, like wedding anniversaries, or professional, such as marking the founding date of a business.",
+        "title": "Anniversary",
+        "category": "A"
 
     },
     {
         "id": 2,
         "imageUrl": "birthjday1.png",
-        "description": "A birthday party is a social gathering or celebration held to commemorate the anniversary of a person's birth.",
+        "description": "A birthday celebrates the anniversary of a person's birth. It's a special occasion often marked by gathering friends and family, giving gifts, and enjoying festivities like cake cutting, games, and dancing. Children’s birthday parties might include themes, clowns, or magicians, while adults may prefer a dinner or a night out. Some people also use this day for reflection and personal goals for the coming year. The celebration can be an intimate gathering or a grand event, depending on personal preferences.",
         "title": "Birthday Party",
         "category": "B"
     }
     ,
     {
         "id": 3,
-        "imageUrl": "birthjday1.png",
-        "description": "A birthday party is a social gathering or celebration held to commemorate the anniversary of a person's birth.",
-        "title": "Birthday Party",
-        "category": "B"
-    }
-    ,
-    {
-        "id": 4,
-        "imageUrl": "birthjday1.png",
-        "description": "A birthday party is a social gathering or celebration held to commemorate the anniversary of a person's birth.",
-        "title": "Birthday Party",
+        "imageUrl": "corporate event.jpg",
+        "description": "Corporate events are organized by businesses to achieve specific goals, such as team building, product launches, training sessions, or networking. These events can range from small meetings to large conferences and can include activities like keynote speeches, workshops, and social gatherings. The aim is to foster a sense of community, share knowledge, and drive business objectives. They often involve careful planning, professional catering, and sometimes entertainment to ensure a productive and enjoyable experience for attendees.",
+        "title": "Corporate Event",
         "category": "C"
     }
     ,
     {
-        "id": 5,
-        "imageUrl": "birthjday1.png",
-        "description": "A birthday party is a social gathering or celebration held to commemorate the anniversary of a person's birth.",
-        "title": "Birthday Party",
+        "id": 4,
+        "imageUrl": "dj.png",
+        "description": "A DJ party is an event where a disc jockey plays recorded music for a crowd, typically at clubs, private parties, or festivals. These events focus on creating a lively and energetic atmosphere, often with a dance floor, lighting effects, and sometimes visual projections. The music can vary widely, including genres like electronic, hip-hop, pop, and rock. DJ parties are popular for both casual gatherings and larger events, providing entertainment that encourages dancing and socializing.",
+        "title": "DJ Party",
         "category": "D"
     }
     ,
     {
-        "id": 6,
-        "imageUrl": "birthjday1.png",
-        "description": "A birthday party is a social gathering or celebration held to commemorate the anniversary of a person's birth.",
-        "title": "Birthday Party",
+        "id": 5,
+        "imageUrl": "engagement.jpg",
+        "description": "An engagement celebration marks the announcement of a couple's intention to marry. This event brings together friends and family to celebrate the couple's commitment. It can be an intimate gathering at home or a larger party at a venue. Engagement parties often include speeches, toasts, and the sharing of the couple’s story. It’s a time for loved ones to offer congratulations and best wishes, and sometimes the couple will exchange engagement rings during the event.",
+        "title": "Engagement Party",
         "category": "E"
-    },
+    }
+    ,
     {
-        "id": 7,
-        "imageUrl": "birthjday1.png",
-        "description": "A birthday party is a social gathering or celebration held to commemorate the anniversary of a person's birth.",
-        "title": "Birthday Party",
+        "id": 6,
+        "imageUrl": "farewell party.jpg",
+        "description": "A farewell party is held to bid goodbye to someone who is leaving, such as a colleague moving to a new job, a friend relocating, or a student graduating. These parties can be emotional as they mark the end of one phase and the beginning of another. Activities often include speeches, sharing memories, and presenting farewell gifts. It’s an opportunity to express gratitude, share well-wishes, and create lasting memories with the person who is departing.",
+        "title": "Farewell Party",
         "category": "F"
     },
     {
-        "id": 8,
-        "imageUrl": "birthjday1.png",
-        "description": "A birthday party is a social gathering or celebration held to commemorate the anniversary of a person's birth.",
-        "title": "Birthday Party",
+        "id": 7,
+        "imageUrl": "get-together.jpg",
+        "description": "A get-together party is an informal event where friends, family, or colleagues gather to catch up and enjoy each other’s company. These parties can be held for various reasons, such as holidays, milestones, or just for fun. Activities can include chatting, playing games, and enjoying food and drinks. The focus is on relaxation and social interaction, making it a great way to strengthen relationships and create new memories.",
+        "title": "Get-Together Party",
         "category": "G"
-    }
-
+    },
+    {
+        "id": 8,
+        "imageUrl": "holi-party.jpg",
+        "description": "A Holi party celebrates the Indian festival of colors, marking the arrival of spring. This vibrant event involves playing with colored powders, water balloons, and music. Participants often wear white clothes that become brightly colored as the celebration progresses. Traditional sweets and drinks, like bhang, are served, and there’s usually dancing and singing. Holi parties are joyous and inclusive, emphasizing the themes of renewal, love, and the triumph of good over evil.",
+        "title": "Holi Party",
+        "category": "H"
+    },
+    {
+        "id": 9,
+        "imageUrl": "inaguration.jpg",
+        "description": "An inauguration celebration marks the official opening or commencement of something significant, such as a building, project, or public office. This event often includes speeches by notable figures, ribbon-cutting ceremonies, and media coverage. It’s a way to introduce the new venture to the public and stakeholders, generating excitement and awareness. Guests typically include VIPs, employees, clients, and the community, and the event can feature tours, entertainment, and refreshments.",
+        "title": "Inaguration Celebration",
+        "category": "I"
+    },
+    {
+        "id": 10,
+        "imageUrl": "janmashtami.jpg",
+        "description": "Janmashtami celebrates the birth of Lord Krishna, a major deity in Hinduism. The festivities often include fasting, singing devotional songs, and enacting scenes from Krishna’s life through dramas and dances. In some regions, people form human pyramids to break a pot of butter, mimicking Krishna’s childhood exploits. Temples and homes are decorated, and special prayers and rituals are conducted at midnight, believed to be the time of Krishna’s birth. It’s a spiritually significant event filled with devotion and joy.",
+        "title": "janmashtami Celebration",
+        "category": "J"
+    },
+    {
+        "id": 11,
+        "imageUrl": "kid's party2.jpg",
+        "description": "A kid's party is designed to celebrate a child’s birthday or a special milestone in a fun and engaging way. These parties often feature themes based on the child’s interests, such as superheroes, princesses, or favorite TV characters. Activities can include games, crafts, a bounce house, and entertainment like clowns or magicians. Food typically includes child-friendly options like pizza, cake, and ice cream. The goal is to create a memorable and enjoyable experience for the children and their friends.",
+        "title": "Kid's Party",
+        "category": "K"
+    },
+    {
+        "id": 12,
+        "imageUrl": "lunch party.jpg",
+        "description": "A lunch party is a social gathering held during midday, often to celebrate an occasion or simply enjoy good company. These events can be formal or informal and typically feature a variety of dishes, including appetizers, main courses, and desserts. Lunch parties can be held at home, in restaurants, or at special venues. They provide an opportunity for socializing, networking, and relaxation in a more casual setting than an evening event.",
+        "title": "Lunch Party",
+        "category": "L"
+    },
+    {
+        "id": 13,
+        "imageUrl": "mother's day.jpg",
+        "description": "A Mother’s Day celebration honors mothers and motherhood. It is typically marked by giving gifts, flowers, and cards to mothers, as well as spending quality time with them. Many families organize special meals, either at home or in restaurants, and engage in activities that the mother enjoys. It’s a day to express love, gratitude, and appreciation for the hard work and sacrifices that mothers make. Schools and communities often hold events and programs to celebrate the occasion.",
+        "title": "Mother's Day Celebration",
+        "category": "M"
+    },
+    {
+        "id": 14,
+        "imageUrl": "newyear-party.jpg",
+        "description": "A New Year party is a celebration to mark the beginning of the new year. These parties often include festive decorations, music, dancing, and fireworks. People gather with friends and family to count down to midnight and celebrate the start of a new year with joy and optimism. Traditional elements include toasts, resolutions, and special foods. New Year parties can range from small gatherings at home to large public events with entertainment and performances.",
+        "title": "New Year Party",
+        "category": "N"
+    },
+    {
+        "id": 15,
+        "imageUrl": "officeparty.jpg",
+        "description": "An office party is a social event organized by a company for its employees, often to celebrate achievements, holidays, or milestones. These parties can help boost morale, foster team spirit, and provide a break from the routine work environment. Activities can include games, awards, speeches, and socializing. Office parties may be held in the office, at a restaurant, or at a special venue. They offer an opportunity for employees to interact in a relaxed setting and build stronger relationships.",
+        "title": "Office Party",
+        "category": "O"
+    },
+    {
+        "id": 16,
+        "imageUrl": "potluck.jpg",
+        "description": "A potluck party is a social gathering where each guest contributes a dish to be shared among everyone. This type of event encourages variety and allows guests to showcase their cooking skills. Potluck parties are often informal and can be held at homes, community centers, or outdoor spaces. The shared responsibility for the meal creates a communal atmosphere and reduces the hosting burden. It’s a great way to enjoy diverse cuisines and foster a sense of community.",
+        "title": "Potluck Party",
+        "category": "P"
+    },
+    {
+        "id": 17,
+        "imageUrl": "",
+        "description": "A quiz hosting event usually begins with planning and preparation, including choosing the theme or topics for the quiz, creating a set of questions, and determining the format (e.g., multiple-choice, true or false, short answer). The event can be hosted in person at venues like community centers, schools, offices, or pubs, or virtually using video conferencing platforms.The quizmaster plays a crucial role, guiding participants through the rounds, asking questions, providing hints if needed, and keeping the energy high. ",
+        "title": "Quiz Competition",
+        "category": "Q"
+    },
+     {
+        "id": 18,
+        "imageUrl": "reunion party.jpg",
+        "description": "A reunion celebration brings together people who have shared experiences or relationships, such as classmates, family members, or colleagues. These events are an opportunity to reconnect, share memories, and catch up on each other’s lives. Reunions can include activities like speeches, photo slideshows, games, and socializing. They can be held at various venues, from homes to banquet halls, and often involve planning committees to organize the details. The goal is to strengthen bonds and relive fond memories.",
+        "title": "Reunion Celebration",
+        "category": "R"
+    },
+    {
+        "id": 19,
+        "imageUrl": "sangeet.jpg",
+        "description": "A sangeet function is a pre-wedding celebration in Indian culture, particularly among Punjabis and Gujaratis. It involves music, dancing, and singing, with performances by family and friends. The event is typically held a few days before the wedding and serves as a way for both families to come together and celebrate. The atmosphere is festive and joyous, with elaborate decorations, traditional attire, and a focus on entertainment. It’s a highlight of the wedding festivities.",
+        "title": "Sangeet Function",
+        "category": "S"
+    },
+    {
+        "id": 20,
+        "imageUrl": "tradeshow.jpg",
+        "description": "A trade show is an event where businesses in a specific industry showcase their products and services to potential customers, partners, and industry professionals. These events feature exhibition booths, product demonstrations, and networking opportunities. Trade shows can include seminars, workshops, and keynote speeches to provide insights into industry trends and innovations. They are an excellent platform for generating leads, building brand awareness, and forming business relationships.",
+        "title": "Trade Show",
+        "category": "T"
+    },
+    {
+        "id": 21,
+        "imageUrl": "Unveiling event.jpg",
+        "description": "An unveiling event is a formal gathering to reveal something new, such as a product, artwork, or project. These events often include presentations, speeches, and demonstrations to highlight the significance and features of the item being unveiled. Media coverage and invitations to key stakeholders are common to generate buzz and interest. The event can be held at a relevant venue, such as a gallery for an art unveiling or a company headquarters for a product launch.",
+        "title": "Unveiling Event",
+        "category": "U"
+    },
+    {
+        "id": 22,
+        "imageUrl": "valentine's day.jpg",
+        "description": "Valentine’s Day is celebrated on February 14th and is dedicated to expressing love and affection between intimate partners. Common activities include exchanging cards, flowers, and gifts, as well as enjoying romantic dinners or outings. The day is marked by themes of love, romance, and appreciation, with many people planning special surprises or getaways for their significant others. It’s a popular occasion for marriage proposals and other romantic gestures.",
+        "title": "Valentine's Day",
+        "category": "V"
+    },
+    {
+        "id": 23,
+        "imageUrl": "wedding.png",
+        "description": "A wedding is a ceremony where two people are united in marriage. This significant life event is often accompanied by various pre-wedding and post-wedding celebrations, including engagements, sangeet, and receptions. Weddings can be elaborate, with rituals, customs, and traditions varying widely across cultures and religions. The ceremony typically involves vows, the exchange of rings, and the presence of family and friends. Weddings are a joyous occasion marked by festivities, food, music, and dance.",
+        "title": "Wedding",
+        "category": "W"
+    },
+    {
+        "id": 24,
+        "imageUrl": "X-mas (Christmas).jpg",
+        "description": "Christmas, celebrated on December 25th, marks the birth of Jesus Christ. It is a major holiday for Christians and widely celebrated in many parts of India. Traditions include decorating Christmas trees, attending church services, singing carols, and exchanging gifts. Families gather for festive meals, and homes are adorned with lights and ornaments. The spirit of Christmas emphasizes love, joy, and goodwill, making it a time for communal gatherings and spreading cheer.",
+        "title": "X-mas Celebration",
+        "category": "X"
+    },
+    {
+        "id": 25,
+        "imageUrl": "year end party.jpg",
+        "description": "A year-end party is held to celebrate the end of the year and reflect on the accomplishments and challenges of the past twelve months. These events are often organized by businesses, social groups, or families. Activities can include speeches, award ceremonies, entertainment, and dancing. It’s a time to unwind, enjoy, and make resolutions for the coming year. Year-end parties are typically festive and joyous, marking a transition and new beginnings.",
+        "title": "Year End Party",
+        "category": "Y"
+    },
+    {
+        "id": 26,
+        "imageUrl": "zoo birthday party.jpg",
+        "description": "A zoo birthday party is a unique and exciting way to celebrate a child’s birthday, held at a zoo. The event can include guided tours, animal encounters, and educational activities. Party guests might have the opportunity to feed or interact with certain animals, and there are usually themed decorations and games. Zoos often provide party packages that include food, drinks, and sometimes a dedicated party area. It’s an engaging and memorable experience for children and their friends.",
+        "title": "Zoo Birthday Party",
+        "category": "Z"
+    },
+    
 ]
